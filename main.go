50226d357a4215d5b5e2127abd27d40d6a257c6c
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-08-24 13:38:36 +0530

    adding waitgroups to main

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"sync"
+
 	"github.com/sankalp/quantum_safe_tls/tls"
 )
 
 func main(){
-	go tls.Server()
-	go tls.Client()
+    var wg sync.WaitGroup
+    wg.Add(2)
+
+    go func() {
+        defer wg.Done()
+        tls.Server()
+    }()
+
+    go func() {
+        defer wg.Done()
+        tls.Client()
+    }()
+
+    wg.Wait()
 }
